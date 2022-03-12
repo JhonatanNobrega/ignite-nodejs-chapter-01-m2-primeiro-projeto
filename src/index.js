a81af219bf57e2fabef501d0f4c36499a0796a34
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2022-03-12 00:44:53 -0300

    Extrado da conta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
@@ -15,14 +15,14 @@ const customers = [];
  */
 app.post('/account', (req, res) => {
 
-  const {cpf, name} = req.body;
+  const { cpf, name } = req.body;
 
   const customerAlreadyExists = customers.some(
     (customer) => customer.cpf === cpf
   );
 
-  if(customerAlreadyExists){
-    return res.status(400).json({ 
+  if (customerAlreadyExists) {
+    return res.status(400).json({
       error: 'Customer already exists!'
     })
   }
@@ -37,5 +37,12 @@ app.post('/account', (req, res) => {
   return res.status(201).send();
 })
 
+app.get('/statement/:cpf', (req, res) => {
+  const { cpf } = req.params;
+  const customer = customers.find(
+    (customer) => customer.cpf === cpf
+  );
+  return res.json(customer.statement);
+})
 
 app.listen(3333);
